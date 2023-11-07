bc0ed5a4443f36b0269edcafd47185d19fecb78c
drebyrd715 <drebyrd715@gmail.com>
2023-11-07 14:26:40 -0500

    figuring out img problem

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ const otherPiece = otherPiece; /// tile I want to swap with ///
 ///// create number turns///////
 const turns = 0;
 
+window.onload = function() {
+    // start the gameboard/// need loop //////
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            // create tile img /////
+            let piece = document.createElement("img");
+            //// make dummy imgs for game/////
+            piece.src = "dummy.png";
+            document.querySelector("#gameBoard").append(piece);
+        }
+    }
+}
