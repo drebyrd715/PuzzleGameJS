49b9d68e3e39b8f6f71cab3927b5f66140a78cd5
drebyrd715 <drebyrd715@gmail.com>
2023-11-07 14:56:54 -0500

    working almost

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 const rows =5;
 const columns = 5;
 /////////// create variables//////////
-const currentPiece = currentPiece; /// tile I click to drag///
-const otherPiece = otherPiece; /// tile I want to swap with ///
+let currentPiece /// tile I click to drag///
+let otherPiece  /// tile I want to swap with ///
 ///// create number turns///////
 const turns = 0;
 
@@ -13,9 +13,11 @@ window.onload = function() {
         for (let c = 0; c < columns; c++) {
             // create tile img /////
             let piece = document.createElement("img");
-            //// make dummy imgs for game/////
+            // make dummy imgs for game/////
             piece.src = "dummy.png";
             document.querySelector("#gameBoard").append(piece);
         }
     }
+
+    //Pieces///
 }
