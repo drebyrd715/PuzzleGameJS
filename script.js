1c21fed5bc3253015f1b5bb39c334bf809ef1c6f
drebyrd715 <drebyrd715@gmail.com>
2023-11-07 15:20:28 -0500

    figure out how fix my problem

--- script.js
+++ script.js
@@ -12,12 +12,22 @@ window.onload = function() {
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < columns; c++) {
             // create tile img /////
-            let piece = document.createElement("img");
+            let block = document.createElement("img");
             // make dummy imgs for game/////
-            piece.src = "dummy.png";
-            document.querySelector("#gameBoard").append(piece);
+            block.src = "dummy.png";
+            document.querySelector("#gameBoard").append(block);
         }
     }
 
     //Pieces///
+    let pieces = [];
+    for (let i=1; i <= rows*columns; i++){
+        pieces.push(i.toString()); /// put "1" to "25" into arr (puzzle images name)     
+    }
+    //// insert puzzle pieces in bottom section///////////
+    // for (let i = 0; i < pieces.length; i++){
+    //     let block = document.createElement("img");
+    //     block.src = "./images" + pieces[i] + ".png";
+    //     document.querySelector("puzzPieces").append(block)
+    // }
 }
