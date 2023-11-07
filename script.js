d0e1ca45f08751cd7708992f31b9b8db15eb79a6
drebyrd715 <drebyrd715@gmail.com>
2023-11-07 14:08:19 -0500

    variables

---
+++ script.js
@@ -0,0 +1,9 @@
+////// get my rows and columns////////
+const rows =5;
+const columns = 5;
+/////////// create variables//////////
+const currentPiece = currentPiece; /// tile I click to drag///
+const otherPiece = otherPiece; /// tile I want to swap with ///
+///// create number turns///////
+const turns = 0;
+
