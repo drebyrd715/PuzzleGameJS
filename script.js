c349b1f3a072d4232abafe1506e9c1fc3caf6d07
drebyrd715 <drebyrd715@gmail.com>
2023-11-09 10:21:15 -0500

    got pics up top

--- script.js
+++ script.js
@@ -12,23 +12,25 @@ window.onload = function() {
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < columns; c++) {
             // create tile img /////
-            let block = document.createElement("img");
+            let tile = document.createElement("img");
             // make dummy imgs for game/////
-            block.src = "./images/dummy.png";
-            document.querySelector("#gameBoard").append(block);
+            tile.src = "picturefolder/empty1.jpg";
+            tile.style.height = "80px"
+            document.querySelector("#gameBoard").append(tile);
         }
     }
-
-    //Pieces///
+}
+    // pieces///
     let pieces = [];
     for (let i=1; i <= rows*columns; i++){
         pieces.push(i.toString()); /// put "1" to "25" into arr (puzzle images name)     
     }
+
     // insert puzzle pieces in bottom section///////////
     for (let i = 0; i < pieces.length; i++){
-        let block = document.createElement("img");
-        block.src = "./picturefolder" + pieces[i] + ".jpg";
-        document.querySelector("puzzPieces").append(block)
+        let tile = document.createElement("img");
+        tile.src = "picturefolder" + pieces[i] + ".jpg";
+        document.querySelector("puzzPieces").append(tile)
     }
-}
+
 
